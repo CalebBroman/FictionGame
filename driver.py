4c8d2bfbdb6f89435da47c285dd4fcfc5a5f3404
MisterBioMan <caleb.broman@gmail.com>
2025-05-12 16:24:49 -0500

    Added actual game functionality
    Guesses can now be made and clues are given back. The user is given as many guesses as needed until they get the correct word. No time limits, restrictions on guessed words. (except that it must be 5 letters long) Words can be repeated, which will be fixed at some point.

--- driver.py
+++ driver.py
@@ -10,6 +10,7 @@ while True:
         print("Choose a difficulty:")
         for i in difficultyOptions:
             print(i)
+        print()
         userVal = input("> ")
         if userVal in difficultyOptions:
             difficulty = userVal
@@ -17,12 +18,48 @@ while True:
         else:
             print("Invalid input, please try again.")
             print()
-    #Generate new word
+    #Get random word
     word = wordHandler.chooseWord(difficulty)
-    print(word)
 
-
-
+    #User guesses and clues back
+    guesses = []
+    while True:
+        userVal = ""
+        #Get the user's guess
+        while not (len(userVal) == 5 and userVal.isalpha()):
+            print("Please enter a 5 letter word as a guess:")
+            print()
+            userVal = input("> ")
+            print()
+            if not (len(userVal) == 5 and userVal.isalpha()):
+                print("Invalid input, please try again.")
+                print()
+        userGuess = userVal.casefold()
+        #check if user is correct
+        if userGuess != word:
+            #get correct set of clues:
+            trueClues = wordHandler.trueClues(word, userGuess)
+            actualClues = wordHandler.clueStrategy(trueClues)
+            guesses.append((userGuess, actualClues[0], actualClues[1]))
+            print("Your Guess: " + userGuess)
+            print("Clues:      " + actualClues[0])
+            print()
+            print()
+        else:
+            print("Correct!")
+            print()
+            break
+    print("Guess overview:")
+    print("Total guesses: " + len(guesses))
+    print()
+    for i in range(len(guesses)):
+        truthStr = "TTTTT"
+        truthStr = truthStr[:guesses[i][2]] + 'F' + truthStr[guesses[i][2]+1:]
+        print(".")
+        print("Guess: " + guesses[i][0])
+        print("Clues: " + guesses[i][1])
+        print("Truth: " + truthStr)
+        print()
 
     #Ask to exit when done
     valid = False
--- wordHandler.py
+++ wordHandler.py
@@ -1,8 +1,54 @@
 import random
 
 def chooseWord(listName):
+    #Read word list
     with open(listName + ".txt") as fp:
         wordList = fp.readlines()
+    #Choose random word
     rval = random.random()
     word = wordList[int(rval * len(wordList))]
     return word
+
+
+def trueClues(word, guess):
+    #Find correct set of clues
+    #Clue values:
+    #'X' -> letter not in word
+    #'~' -> letter in word, but wrong position
+    #'V' -> letter in correct position
+    clueSet = ""
+    for i in range(5):
+        if word[i] == guess[i]:
+            clueSet += 'V'
+        elif word.count(guess[i]) > guess[:i-1].count(guess[i]):
+            clueSet += '~'
+        else:
+            clueSet += 'X'
+    return clueSet
+
+
+def clueStrategy(trueClues):
+    #Choose random index
+    rval = random.random()
+    idx = int(rval * 5)
+    #Get the correct clue at that index, convert to number
+    if trueClues[idx] == 'X':
+        tVal = 0
+    elif trueClues[idx] == '~':
+        tVal = 1
+    else:
+        tVal = 2
+    #change that value
+    rval = random.random()
+    tVal = (tVal + int(rval * 2) + 1) % 3
+    print(tVal)
+    #Rebuild clue string
+    if tVal == 0:
+        actualClues = trueClues[:idx] + 'X' + trueClues[idx+1:]
+    elif tVal == 1:
+        actualClues = trueClues[:idx] + '~' + trueClues[idx+1:]
+    else:
+        actualClues = trueClues[:idx] + 'V' + trueClues[idx+1:]
+
+    #return the clues and the index that was changed
+    return (actualClues, idx)
